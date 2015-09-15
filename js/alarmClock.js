d180a6895b0d2eadc935c5ea1ff45918b0952807
Wayne Hansen <whansen@Zips-MacBook-Pro-57.local>
2015-09-14 23:50:21 -0400

    Added comments.

--- js/alarmClock.js
+++ js/alarmClock.js
@@ -1,3 +1,4 @@
+// Adds '0' strings to hours, minutes, seconds values for formatting (00:00:00)
 function addZero(value) {
     if(value < 10) {
         return '0' + value;
@@ -6,15 +7,24 @@ function addZero(value) {
     }
 }
 
+// Creates date object and prints formatted time to 'result' area.
 function alarmClockTime () {
 	var currentTime = new Date();
 	var hour = currentTime.getHours();
 	var minutes = currentTime.getMinutes();
 	var seconds = currentTime.getSeconds();
-	var meridiem = currentTime.getHours() > 12 ? "PM" : "AM";
+	
+	// Ternary operator (?) for conditional statement to get meridiem.
+	var meridiem = currentTime.getHours() >= 12 ? "PM" : "AM";
+	
+	// Concatenated time string.
 	var currentTimeDisplay = addZero(hour) + ":" + addZero(minutes) + ":" + addZero(seconds) + " " + meridiem;
+	
+	// CSS styling in JS?
 	document.getElementById('result').style.color = "white";
+
 	document.getElementById('result').innerHTML = currentTimeDisplay;
 }
 
+// setInterval() runs function every x milliseconds (e.g. 1000 ms = 1 second).
 setInterval(function () {alarmClockTime()}, 1000);
